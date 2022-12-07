612645525ade4741dd11b5acb089d53f53eed4e6
koladeg <tayokay@yahoo.com>
2022-12-07 14:11:23 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Layout/>,
+        children:[
+            {
+                path:"/",
+                element:<Home />
+            },
+            {
+                path:"/product/:id",
+                element:<Products/>
+            },
+            {
+                path:"/product/:id",
+                element:<Product/>
+            },
+        ]
+    },
+])
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <Outlet />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
